4a698d5d89c6464bd4e34fa34d55a5908f0791cb
debuggingdan <git@debuggingdan.com>
2025-03-10 22:18:38 +0100

    chore: rename to debuggingdan

--- common.js
+++ common.js
@@ -140,6 +140,8 @@ module.exports = {
         },
       },
     ],
+    /* Always require curly braces */
+    curly: 'error',
   },
   overrides: [
     {
--- preact.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: ['eslint-config-preact', './common'].map(require.resolve),
-};
